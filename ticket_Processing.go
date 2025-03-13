c26a9f597a3e3c878f8481f9a85225b58c2891ac
manoty <kevinnoty21@gmail.com>
2025-03-13 16:57:20 +0300

    added order validation

--- ticket_Processing.go
+++ ticket_Processing.go
@@ -19,11 +19,31 @@ func placeOrder() <-chan ticketOrder{
 
 		for i := 0; i < len(customers); i++{
 			order := ticketOrder{customerName: customers[i], ticketRequested: tickets[i], orderStatus: "Pending"}
-			fmt.println("order placed: %s wants %d tickets\n", order.customerName, order.ticketRequested)
+			fmt.Printf("order placed: %s wants %d tickets\n", order.customerName, order.ticketRequested)
 			out <-order
 			time.Sleep(500 * time.Millisecond) //simulate time delay
 
 	}
 	close(out)}()
 	return out
+}
+//Order Validation
+func validateOrder(in <-chan ticketOrder) <-chan ticketOrder{
+	out := make(chan ticketOrder)
+	go func(){
+		for order := range in{
+			if order.ticketRequested <= totalTickets{
+				order.orderStatus = "Valid"
+				totalTickets -= order.ticketRequested
+			}else{
+				order.orderStatus = "Rejected(not enough tickets)"
+
+			}
+				out <- order
+				time.Sleep(500 * time.Millisecond) //simulate processing time
+		}
+		close(out)
+	}()
+	return out
+			
 }
